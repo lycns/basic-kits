3065600837943ce396163cd4616d53ce1a73a2a9
lynnkoo <lynnkoo.1120@gmail.com>
2020-12-03 12:04:06 +0800

    update inner height

--- packages/dom/src/modules/window.ts
+++ packages/dom/src/modules/window.ts
@@ -1,14 +1,41 @@
-import { DeviceTypes } from './alias'
+import { DeviceTypes, BroswerTypes } from './alias'
 
-export function initInnerHeight(rootNode: HTMLElement | null) {
-    if (!rootNode) {
-        return false
+// 确保 mobile 端容器最小高度在各个平台下一致
+export function initWindowHeight(rootId: string, lock = false) {
+    if (!DeviceTypes.isMobile && !rootId) {
+        return
     }
-    // windows 如果不减去两个像素就会出现滚动条
-    const heightOffset = DeviceTypes.isWindows ? -2 : 0
-    const innerHeight = window.innerHeight
-    rootNode.style.minHeight = innerHeight ? innerHeight + heightOffset  + 'px' : '100vh'
-    return true
+    const rootNode = document.getElementById(rootId)
+    if (!rootNode) {
+        return
+    }
+
+    const initInnerHeight = (rootNode: HTMLElement) => {
+        // windows 如果不减去两个像素就会出现滚动条
+        const heightOffset = DeviceTypes.isWindows ? -2 : 0
+        const innerHeight = window.innerHeight
+        const height = innerHeight ? innerHeight + heightOffset  + 'px' : '100vh'
+        if (!lock) {
+            rootNode.style.minHeight = height
+        } else {
+            rootNode.style.height = height
+        }
+        
+    }
+
+    if (!rootNode.style.minHeight) {
+      // 初始化 min height， 主要目的为兼容 safari 的 innerHeight
+      initInnerHeight(rootNode)
+    } else if (BroswerTypes.isWechatWebview) {
+      // WORKAROUND 兼容 wechat 内置浏览器路由切换时 innerHeight 不一致的问题, 路由延迟大概 100 ms
+      // 即 100ms 后才能取得正确的高度, 未测试 resize 是否能正确触发
+      setTimeout(() => {
+        initInnerHeight(rootNode)
+      }, 100)
+    }
+    window.addEventListener('resize', () => {
+        initInnerHeight(rootNode)
+    })
 }
 
 // 初始化窗口宽度, web 端非响应式兼容时使用，保证窗口自动缩放
